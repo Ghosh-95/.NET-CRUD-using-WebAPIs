b3274ef71271e06aa2acfe302e2004a6a26b87e8
Sushovan <sushovanghosh960@gmail.com>
2025-04-10 18:32:04 +0530

    implement update & delete

--- CRUD_App-using Web API/Controllers/StudentController.cs
+++ CRUD_App-using Web API/Controllers/StudentController.cs
@@ -52,5 +52,102 @@ namespace CRUD_App_using_Web_API.Controllers
             }
             return View();
         }
+
+
+        // UPDATE view
+        [HttpGet]
+        public IActionResult UpdateStudent(int Id)
+        {
+            Student student = new Student();
+            HttpResponseMessage httpResponseMsgUpdate = _httpClient.GetAsync($"{_APIBaseUrl}/{Id}").Result;
+
+            if (httpResponseMsgUpdate.IsSuccessStatusCode)
+            {
+                string result = httpResponseMsgUpdate.Content.ReadAsStringAsync().Result;
+                var selectedStudent = JsonConvert.DeserializeObject<Student>(result);
+
+                if (selectedStudent != null)
+                {
+                    student = selectedStudent;
+                }
+            }
+
+            //return RedirectToAction("Index");
+            return View(student);
+        }
+
+        // UPDATE data
+        [HttpPost]
+        public IActionResult UpdateStudent(Student selectedStudent)
+        {
+            string data = JsonConvert.SerializeObject(selectedStudent);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage updatedResponse = _httpClient.PutAsync($"{_APIBaseUrl}/{selectedStudent.Id}", content).Result;
+
+            if (updatedResponse.IsSuccessStatusCode)
+            {
+                TempData["Sucessfully_updated_msg"] = $"The student (ID:{selectedStudent.Id}) has been successfully updated.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        // Details view
+        [HttpGet]
+        public IActionResult Details(int Id)
+        {
+            Student student = new Student();
+            HttpResponseMessage httpResponseMsgUpdate = _httpClient.GetAsync($"{_APIBaseUrl}/{Id}").Result;
+
+            if (httpResponseMsgUpdate.IsSuccessStatusCode)
+            {
+                string result = httpResponseMsgUpdate.Content.ReadAsStringAsync().Result;
+                var selectedStudent = JsonConvert.DeserializeObject<Student>(result);
+
+                if (selectedStudent != null)
+                {
+                    student = selectedStudent;
+                }
+            }
+
+            return View(student);
+        }
+
+        // Delete view
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            Student student = new Student();
+            HttpResponseMessage httpResponse = _httpClient.GetAsync($"{_APIBaseUrl}/{Id}").Result;
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                string result = httpResponse.Content.ReadAsStringAsync().Result;
+                var selectedStudent = JsonConvert.DeserializeObject<Student>(result);
+                if (selectedStudent != null)
+                {
+                    student = selectedStudent;
+                }
+            }
+
+            return View(student);
+        }
+
+
+        // Renamed the second Delete method to DeleteConfirmed to resolve the CS0111 error
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int Id)
+        {
+            HttpResponseMessage deleteResponse = _httpClient.DeleteAsync(_APIBaseUrl + "/" + Id).Result;
+            if (deleteResponse.IsSuccessStatusCode)
+            {
+                TempData["Delete_success_msg"] = $"The student (ID:{Id}) has been successfully deleted.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
     }
 }
